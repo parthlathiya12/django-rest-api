3352308f67c50c3eef4253e5654802135a6c85d1
Parth Lathiya <parthlathiyaiitb@gmail.com>
2025-05-20 12:57:21 -0400

    Update Docker configuration and views

--- myapp/views.py
+++ myapp/views.py
@@ -7,10 +7,14 @@ from .models import User
 
 class UserView(APIView):
     def get(self, request, *args, **kwargs):
-        try:
-            user = User.objects.filter(email=request.GET['email']).values()
-        except User.DoesNotExist:
-            user = {}
+        email = request.GET.get('email')
+        if not email:
+            return Response({'error': 'Email parameter is required'}, status=400)
+        
+        user = User.objects.filter(email=email).values()
+        if not user:
+            return Response({'error': 'User not found'}, status=404)
+        
         return Response(user)
 
     def post(self, request, *args, **kwargs):
